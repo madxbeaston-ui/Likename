960d7127d01b8bde455d31e8e838987daa06ef20
Ahmad Khan <227086235+madxbeaston-ui@users.noreply.github.com>
2026-04-18 23:12:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -122,10 +122,10 @@ def make_request(encrypt, server_name, token):
     try:
         if server_name == "PK":
             url = "https://clientbp.ggblueshark.com/GetPlayerPersonalShow"
-        elif server_name in {"BD", "IND", "SAC", "NA"}:
-            url = "https://client.ind.freefiremobile.com/GetPlayerPersonalShow"
+        elif server_name in {"BD", "SAC", "NA"}:
+            url = "https://clientbp.ggblueshark.com/GetPlayerPersonalShow"
         else:
-            url = "https://client.us.freefiremobile.com/GetPlayerPersonalShow"
+            url = "https://client.ind.freefiremobile.com/GetPlayerPersonalShow"
         edata = bytes.fromhex(encrypt)
         headers = {
             'User-Agent': "Dalvik/2.1.0 (Linux; U; Android 9; ASUS_Z01QD Build/PI)",
@@ -228,9 +228,9 @@ def handle_requests():
             if server_name_used == "PK":
                 url = "https://clientbp.ggblueshark.com/LikeProfile"
             elif server_name_used in {"BD", "IND", "SAC", "NA"}:
-                url = "https://client.ind.freefiremobile.com/LikeProfile"
+                url = "https://clientbp.ggblueshark.com/LikeProfile"
             else:
-                url = "https://client.us.freefiremobile.com/LikeProfile"
+                url = "https://client.ind.freefiremobile.com/LikeProfile"
 
             asyncio.run(send_multiple_requests(uid, server_name_used, url))
 
