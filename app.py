0373ef89c3e61f1d73d7cbf54adab5df5d3f1c7b
Ahmad Khan <227086235+madxbeaston-ui@users.noreply.github.com>
2026-04-19 22:51:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -16,14 +16,14 @@ app = Flask(__name__)
 
 def load_tokens(server_name):
     try:
-        if server_name == "IND":
-            with open("token_ind.json", "r") as f:
+        if server_name == "PK":
+            with open("token_pk.json", "r") as f:
                 tokens = json.load(f)
         elif server_name in {"BD", "SG", "ME", "VN", "ID", "EU", "PK"}:
             with open("token_pk.json", "r") as f:
                 tokens = json.load(f)
         else:
-            with open("token_ind.json", "r") as f:
+            with open("token_pk.json", "r") as f:
                 tokens = json.load(f)
         return tokens
     except Exception as e:
@@ -120,12 +120,12 @@ def enc(uid):
 
 def make_request(encrypt, server_name, token):
     try:
-        if server_name == "IND":
-            url = "https://client.ind.freefiremobile.com/GetPlayerPersonalShow"
-        elif server_name in {"BD", "SG", "ME", "VN", "ID", "EU", "PK"}:
+        if server_name == "PK":
+            url = "https://clientbp.ggblueshark.com/GetPlayerPersonalShow"
+        elif server_name in {"BD", "SG", "ME", "VN", "ID", "EU"}:
             url = "https://clientbp.ggblueshark.com/GetPlayerPersonalShow"
         else:
-            url = "https://client.ind.freefiremobile.com/GetPlayerPersonalShow"
+            url = "https://clientbp.ggblueshark.com/GetPlayerPersonalShow"
         edata = bytes.fromhex(encrypt)
         headers = {
             'User-Agent': "Dalvik/2.1.0 (Linux; U; Android 9; ASUS_Z01QD Build/PI)",
@@ -225,12 +225,12 @@ def handle_requests():
                 before_like = 0
             app.logger.info(f"Likes before command: {before_like}")
 
-            if server_name_used == "IND":
-                url = "https://client.ind.freefiremobile.com/LikeProfile"
-            elif server_name_used in {"BD", "SG", "ME", "VN", "ID", "EU", "PK"}:
+            if server_name_used == "PK":
+                url = "https://clientbp.ggblueshark.com/LikeProfile"
+            elif server_name_used in {"BD", "SG", "ME", "VN", "ID", "EU"}:
                 url = "https://clientbp.ggblueshark.com/LikeProfile"
             else:
-                url = "https://client.ind.freefiremobile.com/LikeProfile"
+                url = "https://clientbp.ggblueshark.com/LikeProfile"
 
             asyncio.run(send_multiple_requests(uid, server_name_used, url))
 
